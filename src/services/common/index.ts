ff84d87891ebceab3cced4700ffbb8869d126bc1
Niels Krijnen <niels.krijnen@icloud.com>
2024-10-31 20:50:42 +0100

    Fix error handling

--- src/services/common/index.ts
+++ src/services/common/index.ts
@@ -11,34 +11,41 @@ export class WTicketCommon extends WTicketBase {
     limit?: number
     offset?: number
   }): Promise<{ relation?: RelationSearch, relations?: RelationSearch[] }> {
-    const name = "relationLookup";
-    const body = {
-      searchKey: params?.searchKey,
-      includeNonDebtors: true,
-      ...params
-    }
-    const xml = this.xmlParse(await this.request(name, body, "com"), name);
-    let xmlRelations = xml["ns2:suggestions"]["ns2:suggestion"] as RelationSearchXML[] | RelationSearchXML;
-    if (Object.prototype.toString.call(xmlRelations) === "[object Array]") {
-      xmlRelations = xmlRelations as RelationSearchXML[];
-      const relations: RelationSearch[] = [];
-      for (const relation of xmlRelations) {
-        relations.push({
-          id: relation["ns2:gc1rel_unid"],
-          searchKey: relation["ns2:searchKey"],
-        })
+    try {
+      const name = "relationLookup";
+      const body = {
+        searchKey: params?.searchKey,
+        includeNonDebtors: true,
+        ...params
       }
+      const xml = this.xmlParse(await this.request(name, body, "com"), name);
+      let xmlRelations = xml["ns2:suggestions"]["ns2:suggestion"] as RelationSearchXML[] | RelationSearchXML;
+      if (Object.prototype.toString.call(xmlRelations) === "[object Array]") {
+        xmlRelations = xmlRelations as RelationSearchXML[];
+        const relations: RelationSearch[] = [];
+        for (const relation of xmlRelations) {
+          relations.push({
+            id: relation["ns2:gc1rel_unid"],
+            searchKey: relation["ns2:searchKey"],
+          })
+        }
+        return {
+          relation: undefined,
+          relations
+        }
+      } else {
+        xmlRelations = xmlRelations as RelationSearchXML;
+        return {
+          relation: {
+            id: xmlRelations["ns2:gc1rel_unid"],
+            searchKey: xmlRelations["ns2:searchKey"]
+          } as RelationSearch,
+          relations: undefined
+        }
+      }
+    } catch (error) {
       return {
         relation: undefined,
-        relations
-      }
-    } else {
-      xmlRelations = xmlRelations as RelationSearchXML;
-      return {
-        relation: {
-          id: xmlRelations["ns2:gc1rel_unid"],
-          searchKey: xmlRelations["ns2:searchKey"]
-        } as RelationSearch,
         relations: undefined
       }
     }
